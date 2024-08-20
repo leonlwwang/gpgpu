9a6de7c29adfc0eaf8d877876b2eb9016f414c88
Leon Wang <leon.lw.wang@gmail.com>
2024-08-19 23:23:58 -0500

    part 1 work

--- texture-map/textureMap.js
+++ texture-map/textureMap.js
@@ -11,4 +11,37 @@ export const textureMap = async (canvas) => {
   const vs = 'texture-map/shaders/vertex.glsl'
   const fs = 'texture-map/shaders/fragment.glsl'
   const program = await initShaders(gl, vs, fs)
+
+  const positionLoc = gl.getAttribLocation(program, 'position')
+  const srcTexLoc = gl.getUniformLocation(program, 'srcTex')
+
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    -1, -1,
+    1, -1,
+    -1, 1,
+    -1, 1,
+    1, -1,
+    1, 1,
+  ]), gl.STATIC_DRAW)
+
+  const vao = gl.createVertexArray()
+  gl.bindVertexArray(vao)
+
+  gl.enableVertexAttribArray(positionLoc)
+  gl.vertexAttribPointer(
+    positionLoc,
+    2,
+    gl.FLOAT,
+    false,
+    0,
+    0,
+  )
+
+  const srcWidth = 3
+  const srcHeight = 2
+  const tex = gl.createTexture()
+  gl.bindTexture(gl.TEXTURE_2D, tex)
+  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1)
 }
