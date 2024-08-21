b6da684d4e9b771ff079020c7229b0565eab8c18
Leon Wang <leon.lw.wang@gmail.com>
2024-08-20 19:33:48 -0500

    part 1 simple multiply mapping

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
-import { textureMap } from './texture-map/textureMap'
+import { multArrByTextureMap } from './texture-map/textureMap'
 
 const canvas = document.createElement('canvas')
 document.body.appendChild(canvas)
-textureMap(canvas)
+
+const arr = [1, 2, 3, 4, 5, 6]
+multArrByTextureMap(canvas, arr)
--- texture-map/textureMap.js
+++ texture-map/textureMap.js
@@ -1,11 +1,11 @@
 import { initShaders } from "../common/gl-init"
 
-export const textureMap = async (canvas) => {
-  const outWidth = 3
-  const outHeight = 3
+export const multArrByTextureMap = async (canvas, arr) => {
+  const dstWidth = 3
+  const dstHeight = 2
 
-  canvas.width = outWidth
-  canvas.height = outHeight
+  canvas.width = dstWidth
+  canvas.height = dstHeight
 
   const gl = canvas.getContext('webgl2')
   const vs = 'texture-map/shaders/vertex.glsl'
@@ -44,4 +44,34 @@ export const textureMap = async (canvas) => {
   const tex = gl.createTexture()
   gl.bindTexture(gl.TEXTURE_2D, tex)
   gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1)
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.R8,
+    srcWidth,
+    srcHeight,
+    0,
+    gl.RED,
+    gl.UNSIGNED_BYTE,
+    new Uint8Array(arr),
+  )
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  gl.useProgram(program)
+  gl.uniform1i(srcTexLoc, 0)
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6)
+
+  const results = new Uint8Array(dstWidth * dstHeight * 4)
+  gl.readPixels(0, 0, dstWidth, dstHeight, gl.RGBA, gl.UNSIGNED_BYTE, results)
+
+  const result = []
+  for (let i = 0; i < dstWidth * dstHeight; i++) {
+    result.push(results[i * 4])
+  }
+  console.log(result)
+  return result
 }
