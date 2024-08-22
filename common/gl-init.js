6c044ce7107b0c38767714ca9102ad852a075cde
Leon Wang <leon.lw.wang@gmail.com>
2024-08-21 22:01:02 -0500

    part 2 refactor gl setup

--- common/gl-init.js
+++ common/gl-init.js
@@ -1,18 +1,6 @@
 export const initShaders = async (gl, vs, fs) => {
-  const importShader = async (gl, type, path) => {
-    const source = await fetch(path).then((response) => response.text())
-    const shader = gl.createShader(type)
-    gl.shaderSource(shader, source)
-    gl.compileShader(shader)
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.error(`Failed to compile shader: ${gl.getShaderInfoLog(shader)}`)
-      gl.deleteShader(shader)
-      return null
-    }
-    return shader
-  }
-  const vertexShader = await importShader(gl, gl.VERTEX_SHADER, vs)
-  const fragmentShader = await importShader(gl, gl.FRAGMENT_SHADER, fs)
+  const vertexShader = await initShader(gl, gl.VERTEX_SHADER, vs)
+  const fragmentShader = await initShader(gl, gl.FRAGMENT_SHADER, fs)
 
   const shaderProgram = gl.createProgram()
   gl.attachShader(shaderProgram, vertexShader)
@@ -25,4 +13,15 @@ export const initShaders = async (gl, vs, fs) => {
   }
 
   return shaderProgram
+}
+
+export const initShader = async (gl, type, path) => {
+  const source = await fetch(path).then((response) => response.text())
+  const shader = gl.createShader(type)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(shader))
+  }
+  return shader
 }
--- main.js
+++ main.js
@@ -2,19 +2,19 @@ import './style.css'
 import { addTwosByTextureMap } from './texture-map/addTwos'
 import { multArrByTextureMap } from './texture-map/multArr'
 
-/* part (1.0): multiply 2 over array w/ frag shader */
+/* part (1.0): multiply 2 over array w/ fs */
 const canvasA = document.createElement('canvas')
 document.body.appendChild(canvasA)
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8]
 await multArrByTextureMap(canvasA, arr).then((res) => console.log(res))
 
-/* part (1.1): add every 2 indices w/ frag shader */
+/* part (1.1): add every 2 indices w/ fs */
 const canvasB = document.createElement('canvas')
 document.body.appendChild(canvasB)
 
 await addTwosByTextureMap(canvasB).then((res) => console.log(res))
 
-/* part (2): add, sub, mult 2 arrays w/ tf + fs */
+/* part (2): add, sub, mul 2 arrays w/ tf + fs */
 const canvasC = document.createElement('canvas')
 document.body.appendChild(canvasC)
